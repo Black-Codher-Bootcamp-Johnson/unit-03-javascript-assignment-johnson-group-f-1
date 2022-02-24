e64202bc451cd096bcc0a03914fb647884092787
sade-online <fot.fasan@gmail.com>
2022-02-24 17:42:52 +0000

    user story comments

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// ### User Stories
+
+// 1. **As a** user, **I want to** see the summary of each historical item in a summary card, **so that** I can see a brief introduction of each historical item
+// 1. **As a** user, **I want to** see the summary cards in a timeline in date order, **so that** I can see a brief history (in date order) in one place
+// 1. **As a** user, **I want to** see more details on a particular item in a modal **so that** I can see the full information (including an image)
+// 1. **As a** user, **I want to** dismiss the full details of a single historical item, **so that** I can return to the timeline
+// 1. **As a** developer, **I want to** deploy the changes made to the timeline, **so that** the timeline can be viewed publicly 
